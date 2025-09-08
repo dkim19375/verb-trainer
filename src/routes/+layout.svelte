81ce8734a59a49fb4c00ea2bea4ca4d460a78ec2
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2025-09-07 23:24:09 -0400

    Disable dark reader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import '../app.css';
+	import { browser } from '$app/environment';
 
 	const { children }: { children: Snippet<[]> } = $props();
+
+	let isDarkMode = $state(
+		window.matchMedia('(prefers-color-scheme: dark)').matches,
+	);
+
+	if (browser) {
+		window
+			.matchMedia('(prefers-color-scheme: dark)')
+			.addEventListener('change', function (e: MediaQueryListEvent) {
+				isDarkMode = e.matches;
+			});
+	}
 </script>
+
+<svelte:head>
+	{#if isDarkMode}
+		<meta name="darkreader-lock" />
+	{/if}
+</svelte:head>
 
 {@render children()}
